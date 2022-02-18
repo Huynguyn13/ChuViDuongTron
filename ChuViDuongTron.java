46070e75bc54334e3345f991a7f06e60636945a0
Huynguyn13 <97729515+Huynguyn13@users.noreply.github.com>
2022-02-18 14:25:39 +0700

    Add files via upload

---
+++ ChuViDuongTron.java
@@ -0,0 +1,24 @@
+    /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package chu.vi.duong.tron;
+
+/**
+ *
+ * @author admin
+ */
+import java.util.Scanner;
+public class ChuViDuongTron {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      Scanner sc= new Scanner(System.in);
+      Double r=sc.nextDouble();
+      Double pi=3.14;
+      System.out.println("Chu vi ="+(2*pi*r));
+    }
+    
+}
